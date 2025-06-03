271b9b6e5fbca36cfdd93543f0b1296f59ca32a8
Yang Jie <2593606575@qq.com>
2025-06-03 15:05:28 +0800

    [feature]当前日期显示以及状态栏沉浸式适配

--- app/src/main/java/com/learn/androidhistoryechoes/MainActivity.kt
+++ app/src/main/java/com/learn/androidhistoryechoes/MainActivity.kt
@@ -5,16 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.learn.androidhistoryechoes.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val simpleDateFormat = SimpleDateFormat("MM月dd日", Locale.getDefault())
+        val currentDate = simpleDateFormat.format(Date())
+        binding.tvData.text = currentDate
     }
 }
