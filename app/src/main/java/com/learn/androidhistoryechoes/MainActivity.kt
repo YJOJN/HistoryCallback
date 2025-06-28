148f7dabd73e872df5c0204805fc15ca67107211
Yang Jie <2593606575@qq.com>
2025-06-28 10:21:45 +0800

    [feature]首页增加日期选择器

--- app/src/main/java/com/learn/androidhistoryechoes/MainActivity.kt
+++ app/src/main/java/com/learn/androidhistoryechoes/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.learn.androidhistoryechoes.databinding.ActivityMainBinding
 import com.learn.androidhistoryechoes.view.adapter.HistoryEchoesAdapter
 import com.learn.androidhistoryechoes.viewmodel.MainViewModel
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
     private val adapter = HistoryEchoesAdapter()
+    private var selectedDateTime = System.currentTimeMillis()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         val simpleDateFormat = SimpleDateFormat("MM月dd日", Locale.getDefault())
         val currentDate = simpleDateFormat.format(Date())
         binding.tvData.text = currentDate
+        binding.tvData.setOnClickListener {
+            showDatePickerDialog()
+        }
         binding.rvHistoryEchoes.adapter = adapter
         adapter.setOnItemHistoryEchoesClickListener { view, model ->
             Toast.makeText(this, model.title, Toast.LENGTH_SHORT).show()
@@ -33,6 +38,21 @@ class MainActivity : AppCompatActivity() {
             Log.i(TAG, it.toString())
             adapter.mData = it
         }
+    }
+
+    private fun showDatePickerDialog() {
+        val picker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("选择日期")
+            .setSelection(selectedDateTime)
+            .build()
+        picker.addOnPositiveButtonClickListener {
+            val simpleDateFormat = SimpleDateFormat("MM月dd日", Locale.getDefault())
+            val selectedDate = simpleDateFormat.format(it)
+            mainViewModel.getHistoryEchoesList(selectedDate)
+            binding.tvData.text = selectedDate
+            selectedDateTime = it
+        }
+        picker.show(supportFragmentManager, "DATE_PICKER")
     }
 
     companion object {
